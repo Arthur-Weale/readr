4ca016e3b3acbaa59a67ae25104caa0ae1b18cb9
Arthur Weale <ajayrossy340@gmail.com>
2025-07-07 02:41:44 +0200

    Complete book read check logic using protoype

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const cardContainer = document.querySelector('.card-container');
+const bookCard = document.querySelector('.book-card');
+//const checkboxState = document.querySelector('#readUnreadAttribute');
+
 const myLibrary = [];
 
 function Book( title, author, pages, read){
@@ -8,6 +12,22 @@ function Book( title, author, pages, read){
     this.read = read;
 }
 
+//Creating book prototype to handle read status
+Book.prototype.getCheckedAttribute= function (){
+    return this.read ? 'checked' : '';
+}
+
+Book.prototype.getLabel= function (){
+    return this.read ? 'Read' : 'Unread';
+}
+
+Book.prototype.toggleReadState = function (newId){
+    if(this.id === newId) {
+        this.read = !this.read
+        console.log(this.read)
+    } ;
+}
+
 function addBookToLibrary(title, author, pages, read){
     const newBook = new Book( title, author,  pages, read)
     console.log(newBook.title, newBook.id);
@@ -15,14 +35,40 @@ function addBookToLibrary(title, author, pages, read){
 }
 
 addBookToLibrary( 'The wall', 'Arthur Spwein', 300, true)
-addBookToLibrary( 'The wall secrets', 'Arthur Spwein', 366, true)
-//console.log(myLibrary);
+addBookToLibrary( 'The wall secrets', 'Arthur Spwein', 360, false)
 
 function displayBook(){
     myLibrary.forEach((book)=> {
         console.log(JSON.stringify(book, null, 2))
-        //console.log(book.title)
+        cardContainer.innerHTML += `
+            <div class="book-card">
+            <h3 class="book-title">Title: ${book.title}</h3>
+            <p class="book-author">Author: ${book.author}</p>
+            <p class="book-pages">Page: ${book.pages}</p>
+            <label for="read">${book.getLabel()} <input type="checkbox" class="readUnreadAttribute" data-id='${book.id}' ${book.getCheckedAttribute(book.id)} ></label>
+            <button class="remove-btn">Remove</button>
+            </div>`;
+    //cardContainer.append(bookCard);
     })
 }
 
-displayBook();
+
+function bindCheckboxListeners(){
+    document.querySelectorAll('.readUnreadAttribute').forEach(checkbox =>{
+    checkbox.addEventListener('change', (e)=> {
+        
+                const bookId = e.target.dataset.id;
+                const book = myLibrary.find(book => book.id === bookId)
+                if(book) book.toggleReadState(bookId); 
+                cardContainer.innerHTML = '';
+                //book.toggleReadState(bookId);
+
+                displayBook()
+                bindCheckboxListeners()
+            })
+        } )
+    }
+
+displayBook()
+bindCheckboxListeners()
+
